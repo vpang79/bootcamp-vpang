de39b43536ac6fcef20492b7fae3a3dd4b6fc844
Vincent Pang <vpang23@gmail.com>
2019-11-16 05:11:49 +0000

    luhn challenge completed

--- Luhn/lib/luhn.rb
+++ Luhn/lib/luhn.rb
@@ -1,28 +1,29 @@
 module Luhn
   def self.is_valid?(number)
-    @odd_array = []
-    @even_array = []
+    @new_array = []
     @rev_array = number.digits
 
     @rev_array.each_with_index do |x, y|
       if y.even?
-
-        @even_array.push(x)
+        @new_array.push(x)
       else
         x = 2 * x
         if x > 9
           x = x - 9
         end
-        @even_array.push(x)        
+        
+        @new_array.push(x)        
       end
-      @sum = @even_array.inject(0){|sum, n| sum + n }
+      
+      @sum = @new_array.inject(0){|sum, n| sum + n }
     end
-      if @sum % 10 == 0
-        return true
-      else
-        return false
-      end
+
+    if @sum % 10 == 0
+      return true
+    else
+      return false
     end
   end
+end
 
 
--- Luhn/lib/luhn_test.rb
+++ Luhn/lib/luhn_test.rb
@@ -1,21 +1,20 @@
 class Luhn
   def is_valid?(number)
-    @odd_array = []
-    @even_array = []
+    @new_array = []
     @rev_array = number.digits
 
     @rev_array.each_with_index do |x, y|
       if y.even?
 
-        @even_array.push(x)
+        @new_array.push(x)
       else
         x = 2 * x
         if x > 9
           x = x - 9
         end
-        @even_array.push(x)        
+        @new_array.push(x)        
       end
-      @sum = @even_array.inject(0){|sum, n| sum + n }
+      @sum = @new_array.inject(0){|sum, n| sum + n }
     end
       if @sum % 10 == 0
         return true
@@ -26,5 +25,5 @@ class Luhn
   end
 
 test = Luhn.new
-answer = test.is_valid?(4194560385008504)
+answer = test.is_valid?(4194560385008505)
 print "#{answer}\n"
